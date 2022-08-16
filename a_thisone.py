f038c481124169e2e6e4b8d91b0c8697a9497332
jay-d0 <109148187+jay-d0@users.noreply.github.com>
2022-08-16 21:27:42 +0900

    Update a_thisone.py

--- a_thisone.py
+++ a_thisone.py
@@ -7,4 +7,4 @@ maily_db = maily_crawling('mz세대')
 youtube_db = youtube_crawling('mz세대')
 dc_db = dc_crawling('mz세대')
 
-pd.concat([n_db, pan_db, maily_db, youtube_db, dc_db]).to_csv('DB.csv', index=False)
+pd.concat([n_db, pan_db, maily_db, youtube_db, dc_db]).to_csv('DB_thisone.csv', index=False)
